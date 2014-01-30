c1dd296c707c5079dabc424328d9d9a5fc2d55e8
unknown <dmanning23@hotmail.com>
2014-01-29 19:57:38 -0600

    added some new methods

--- Source/Vector2.cs
+++ Source/Vector2.cs
@@ -87,5 +87,56 @@ namespace Vector2Extensions
 
 			return myVector;
 		}
+
+		/// <summary>
+		/// Given 2 lines in 2D space AB, CD this returns true if an intersection occurs 
+		/// and sets dist to the distance the intersection occurs along AB. 
+		/// Also sets the 2d vector point to the point of intersection
+		/// </summary>
+		/// <param name="A"></param>
+		/// <param name="B"></param>
+		/// <param name="C"></param>
+		/// <param name="D"></param>
+		/// <param name="dist"></param>
+		/// <param name="point"></param>
+		/// <returns></returns>
+		public static bool LineIntersection2D(Vector2 A,
+									   Vector2 B,
+									   Vector2 C,
+									   Vector2 D,
+									   ref float dist,
+									   ref Vector2 point)
+		{
+
+			float rTop = (A.Y - C.Y) * (D.X - C.X) - (A.X - C.X) * (D.Y - C.Y);
+			float rBot = (B.X - A.X) * (D.Y - C.Y) - (B.Y - A.Y) * (D.X - C.X);
+
+			float sTop = (A.Y - C.Y) * (B.X - A.X) - (A.X - C.X) * (B.Y - A.Y);
+			float sBot = (B.X - A.X) * (D.Y - C.Y) - (B.Y - A.Y) * (D.X - C.X);
+
+			if ((rBot == 0) || (sBot == 0))
+			{
+				//lines are parallel
+				return false;
+			}
+
+			float r = rTop / rBot;
+			float s = sTop / sBot;
+
+			if ((r > 0) && (r < 1) && (s > 0) && (s < 1))
+			{
+				dist = Vector2.Distance(A, B) * r;
+
+				point = A + r * (B - A);
+
+				return true;
+			}
+			else
+			{
+				dist = 0;
+
+				return false;
+			}
+		}
 	}
 }
