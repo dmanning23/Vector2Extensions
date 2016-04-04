4d2e4a3f2d04140800a2a3a68aff6e59f8c586a2
Dan Manning <dmanning23@hotmail.com>
2016-04-03 22:20:26 -0500

    nuget packages

--- Vector2Extensions/buildNugetPackages.ps1
+++ Vector2Extensions/buildNugetPackages.ps1
@@ -1,3 +1,3 @@
 nuget pack .\Vector2Extensions.Android\Vector2Extensions.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
-nuget pack .\Vector2Extensions.WindowsGL\Vector2Extensions.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\Vector2Extensions.DesktopGL\Vector2Extensions.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\Vector2Extensions.iOS\Vector2Extensions.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
