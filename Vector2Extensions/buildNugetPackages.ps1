15d6971feb7a81cfd6ef566a589b8f6ffe1fabd4
Dan Manning <dmanning23@hotmail.com>
2016-04-23 00:44:48 -0500

    added winuni proj

--- Vector2Extensions/buildNugetPackages.ps1
+++ Vector2Extensions/buildNugetPackages.ps1
@@ -1,3 +1,5 @@
 nuget pack .\Vector2Extensions.Android\Vector2Extensions.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack .\Vector2Extensions.DesktopGL\Vector2Extensions.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack .\Vector2Extensions.iOS\Vector2Extensions.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\Vector2Extensions.WindowsUniversal\Vector2Extensions.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
