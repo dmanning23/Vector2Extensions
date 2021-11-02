8edfecd512e75a306c6e27a546534821e9962ffb
Dan Manning <dmanning@oceaneering.com>
2021-11-02 16:02:33 -0500

    Added bridge project

--- Vector2Extensions/buildNugetPackages.ps1
+++ Vector2Extensions/buildNugetPackages.ps1
@@ -1,4 +1,5 @@
 rm *.nupkg
 nuget pack .\Vector2Extensions.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\Vector2Extensions.Bridge.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 cp *.nupkg C:\Projects\Nugets\
 nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
